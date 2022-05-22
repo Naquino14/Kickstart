5682ab860c51f16185396c9ea990dc00b710ef59
Nathaniel Aquino <37340195+Naquino14@users.noreply.github.com>
2022-05-22 09:25:16 -0400

    Round C
    A: passed both
    B: im too tired to finish

---
+++ GoogleKickstart/Kickstart 2022/RoundCProblemA.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using c = System.Console;
+using i32 = System.Int32;
+using i64 = System.Int64;
+
+namespace GoogleKickstart2022
+{
+    public class RoundCProblemA
+    {
+        private static readonly char[] spec = { '#', '@', '*', '&' };
+        public static void Mainn()
+        {
+            var tc = int.Parse(c.ReadLine());
+            for (var i = 1; i <= tc; i++)
+                c.WriteLine($"Case #{i}: {GetNewPassword(int.Parse(c.ReadLine()), c.ReadLine())}");
+        }
+
+        private static string GetNewPassword(int n, string s)
+        {
+            var o = s;
+            bool hasLower = false, hasUpper = false, hasDigit = false, hasSpec = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                hasLower |= s[i] >= 0x61 && s[i] <= 0x7A;
+                hasUpper |= s[i] >= 0x41 && s[i] <= 0x5A;
+                hasDigit |= s[i] >= 0x30 && s[i] <= 0x39;
+                hasSpec |= s[i] == 0x23 || s[i] == 0x40 || s[i] == 0x2A || s[i] == 0x26;
+            }
+            var rng = new Random();
+
+            if (!hasLower)
+                o += (char)rng.Next(0x61, 0x7A);
+            if (!hasUpper)
+                o += (char)rng.Next(0x41, 0x5A);
+            if (!hasDigit)
+                o += rng.Next(0, 9);
+            if (!hasSpec)
+                o += spec[rng.Next(0, 3)];
+
+            while (o.Length < 7)
+                o += (char)rng.Next(0x61, 0x7A);
+            return o;
+        }
+    }
+}
---
+++ GoogleKickstart/Kickstart 2022/RoundCProblemB.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using c = System.Console;
+using i32 = System.Int32;
+using i64 = System.Int64;
+using State = System.Object;
+
+namespace GoogleKickstart2022
+{
+    public class RoundCProblemB
+    {
+        public static void Main()
+        {
+            var tc = int.Parse(c.ReadLine());
+            for (var i = 1; i <= tc; i++)
+            { var o = Func(c.ReadLine());  c.WriteLine($"Case #{i}: {(o.p ? $"POSSIBLE\n{o.l}\n{o.ss}" : "IMPOSSIBLE")}"); }
+        }
+        private static (bool p, int? l, string? ss) Func(string s)
+        {
+            (bool p, int? l, string? ss) o = new();
+            var seq = s.Split(' ');
+            int ctxssl = 1;
+            bool r = seq.Length % ctxssl == 0;
+            try
+            {
+                for (int i = 0; i < seq.Length; i++)
+                {
+                    var ctxset = new int[ctxssl];
+                    // im too tired for this sorry
+
+                    ctxssl++;
+                }
+            }
+            catch (IndexOutOfRangeException) { }
+
+            return o;
+        }
+
+        private static int gcf(int a, int b)
+        {
+            while (b != 0)
+            {
+                var t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+    }
+}
